2e44ec15bd8693f7b73a835ca845622a5f79d564
Daniil <trombonil1996@gmail.com>
2022-08-04 17:16:27 +0300

    Daniil -god

--- Ex005_favoritUser/Program.cs
+++ Ex005_favoritUser/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "daniil")
+{
+    Console.WriteLine("Hello, my God");
+}
+
+else
+{
+Console.Write("Hello, ");
+Console.WriteLine(username);
+}
