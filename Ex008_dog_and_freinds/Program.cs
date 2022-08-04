063dd2f4c0b143da82245c65ab65e4260eac3aff
Daniil <trombonil1996@gmail.com>
2022-08-04 22:23:56 +0300

    done_Ex008

--- Ex008_dog_and_freinds/Program.cs
+++ Ex008_dog_and_freinds/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int distance = 10000;
+int firstFrendSpeed = 1;
+int secondFrendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+int timeMh = 0;
+
+int count = 0;
+
+
+while (distance > 10)
+{
+if (friend == 1)
+        {
+        timeMh = distance / (firstFrendSpeed + dogSpeed);
+        friend = 2;
+        }
+
+if (friend == 2)
+        {
+        timeMh = distance / (secondFrendSpeed + dogSpeed);
+        friend = 1;
+        }
+
+
+distance = distance - (firstFrendSpeed + secondFrendSpeed) * timeMh;
+count++;
+}
+
+Console.WriteLine(count);
