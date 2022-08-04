9f7467708692f47b652031d6b74c1a097a763025
Daniil <trombonil1996@gmail.com>
2022-08-04 22:46:58 +0300

    done_0,2_Ex008

--- Ex008_dog_and_freinds/Program.cs
+++ Ex008_dog_and_freinds/Program.cs
@@ -1,30 +1,46 @@
-﻿int distance = 10000;
-int firstFrendSpeed = 1;
-int secondFrendSpeed = 2;
-int dogSpeed = 5;
-int friend = 2;
+﻿Console.Clear();
+int distance = 10015;
+int firstFrendSpeed = 9;
+int secondFrendSpeed = 8;
+int dogSpeed = 11;
+int friend;
 int timeMh = 0;
+string toFriend;
 
 int count = 0;
+
+Console.WriteLine("К какому другу побежит собака, к Первому или Второму?");
+toFriend = Console.ReadLine();
+
+        if (toFriend.ToLower() == "первому")
+                {
+                        friend = 1;      
+                }
+        else 
+                {
+                        friend = 2;
+                }
+
 
 
 while (distance > 10)
 {
-if (friend == 1)
-        {
-        timeMh = distance / (firstFrendSpeed + dogSpeed);
-        friend = 2;
-        }
+        if (friend == 1)
+                {
+                timeMh = distance / (firstFrendSpeed + dogSpeed);
+                friend = 2;
+                }
 
-if (friend == 2)
-        {
-        timeMh = distance / (secondFrendSpeed + dogSpeed);
-        friend = 1;
-        }
+        if (friend == 2)
+                {
+                timeMh = distance / (secondFrendSpeed + dogSpeed);
+                friend = 1;
+                }
 
 
 distance = distance - (firstFrendSpeed + secondFrendSpeed) * timeMh;
 count++;
 }
 
-Console.WriteLine(count);
+
+Console.WriteLine("Собака пробежит в количестве " + count + " раз!");
