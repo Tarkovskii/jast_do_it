89fb3d1898f119a64a5e2719da3d3ca380a81024
Daniil <trombonil1996@gmail.com>
2022-08-04 17:30:11 +0300

    max-mass

--- Ex006_mass/Program.cs
+++ Ex006_mass/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int object1 = 1;
+int object2 = 8;
+int object3 = 6;
+int object4 = 2;
+int object5 = 3;
+
+int max = object1;
+
+if (max < object1) max = object1;
+if (max < object2) max = object2;
+if (max < object3) max = object3;
+if (max < object4) max = object4;
+if (max < object5) max = object5;
+
+Console.Write("max = ");
+Console.WriteLine(max);
